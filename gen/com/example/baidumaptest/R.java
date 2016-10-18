79ace3a374d8896c313a0c42af35629045bf7358
BOB-He1995 <accessfj008@163.com>
2016-10-18 22:04:22 +0800

    baidumap

--- gen/com/example/baidumaptest/R.java
+++ gen/com/example/baidumaptest/R.java
@@ -21,11 +21,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_enemies=0x7f020000;
+        public static final int button_enemies_def=0x7f020001;
+        public static final int button_friends=0x7f020002;
+        public static final int button_friends_def=0x7f020003;
+        public static final int button_locate=0x7f020004;
+        public static final int button_locate_def=0x7f020005;
+        public static final int button_locate_on=0x7f020006;
+        public static final int button_refresh=0x7f020007;
+        public static final int button_refresh_def=0x7f020008;
+        public static final int button_refresh_on=0x7f020009;
+        public static final int centre_radar=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int list_footer=0x7f02000c;
+        public static final int main=0x7f02000d;
+        public static final int radar_header=0x7f02000e;
+        public static final int radar_sweep=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f08000a;
+        public static final int btn_enemies=0x7f080009;
+        public static final int btn_friends=0x7f080008;
+        public static final int btn_locate=0x7f080004;
+        public static final int btn_refresh=0x7f080005;
+        public static final int empty_filler=0x7f080006;
+        public static final int header=0x7f080001;
+        public static final int imageview_sweep=0x7f080003;
         public static final int map_view=0x7f080000;
+        public static final int vgp_center=0x7f080002;
+        public static final int vgp_footer=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
